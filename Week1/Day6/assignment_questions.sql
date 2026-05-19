637a97ca2cbc193140d402006ef353c8c37ee77e
sathviik18 <23pa1a45d1@vishnu.edu.in>
2026-05-19 12:53:36 +0530

    Create assignment_questions.sql

---
+++ Week1/Day6/assignment_questions.sql
@@ -0,0 +1,12 @@
+1. Use a CTE to find employees whose sales exceed the company average.
+2. Create multiple CTEs to calculate customer total spending and rankings.
+3. Write a recursive CTE to generate numbers from 1 to 10.
+4. Use a recursive CTE to display employee hierarchy data.
+5. Create a CTE that filters orders above the average order amount.
+6. Use a CTE and window function together to rank customers by total spending.
+7. Find the second-highest salary in each department.
+8. Display the difference between each employee salary and the department maximum salary.
+9. Combine CTEs and window functions to find the top-performing employee in each department based on total
+sales.
+Bonus Challenge:
+10.Create a report showing monthly sales trends using: CTEs Running totals LAG() Percentage growth calculation
